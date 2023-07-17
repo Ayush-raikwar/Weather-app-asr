3d3d49c170efa15cd82289559eea8b723ce89536
Ayush-raikwar <ayushraikwar45@gmail.com>
2023-07-18 01:01:22 +0530

    finished assignment

--- src/pages/weather-view/WeatherView.js
+++ src/pages/weather-view/WeatherView.js
@@ -1,15 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import { ArrowImg, FlexContainer, FormMain, MainContainer, Separator } from './styles'
+import { ArrowImg, Desc, FlexContainer, FormMain, Location, MainContainer, Separator, SeparatorVertical, Temp, WeatherImg } from './styles'
 import { screenTexts } from '../../constants/screenTexts'
 import ArrowLeft from '../../assets/svg/arrow.svg'
+import axios from 'axios'
+import FeelsLikeIcon from '../../assets/svg/feels-like.svg'
+import HumidityIcon from '../../assets/svg/humidity.svg'
+import LocationIcon from '../../assets/svg/location.svg'
+import { useNavigate } from 'react-router-dom'
+import { routeTexts } from '../../constants/routeTexts'
 
 export const WeatherView = () => {
 
     const [iconUrl, setIconUrl] = useState(null)
+    const city = localStorage.getItem(screenTexts.COMMONS.CITY)
+    const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY
+    const [temp, setTemp] = useState(null)
+    const [country, setCountry] = useState(null)
+    const [description, setDescription] = useState(null)
+    const [feelsLikeTemp, setFeelsLikeTemp] = useState(null)
+    const [humidity, setHumidity] = useState(null)
+    const navigator = useNavigate()
 
     useEffect(() => {
-        fetchWeatherIcon(localStorage.getItem(screenTexts.COMMONS.CITY))
+        fetchWeatherIcon(city)
+        fetchWeatherData()
     }, [])
+
+
+    const kelvinToCelsius = (kelvin) => {
+        return kelvin - 273.15;
+    }
 
     const fetchWeatherIcon = async (city) => {
         try {
@@ -26,15 +46,60 @@ export const WeatherView = () => {
         }
     };
 
+    const fetchWeatherData = async () => {
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+            );
+            console.log(response?.data);
+            const tempVal = kelvinToCelsius(response?.data?.main?.temp)
+            setTemp(tempVal.toFixed())
+            setDescription(response?.data?.weather[0]?.description)
+            setCountry(response?.data?.sys?.country)
+            setFeelsLikeTemp(kelvinToCelsius(response?.data?.main?.feels_like).toFixed())
+            setHumidity(response?.data?.main?.humidity)
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+    }
+
     return (
         <MainContainer>
             <FormMain>
                 <FlexContainer>
-                    <ArrowImg src={ArrowLeft} />
+                    <ArrowImg onClick={() => navigator(routeTexts.HOME)} src={ArrowLeft} />
                     <h3>{screenTexts.HOME.FORM_TITLE}</h3>
                 </FlexContainer>
-                <img src={iconUrl} width={122} height={122} />
                 <Separator />
+                <WeatherImg src={iconUrl} width={122} height={122} />
+
+                <Temp>{temp}°C</Temp>
+
+                <Desc>{description}</Desc>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <img src={LocationIcon} width={14} height={14} />
+                    <Location>{city}, {country}</Location>
+                </div>
+                <Separator />
+
+                <div className='bottom-sec'>
+                    <div className='flex-center'>
+                        <img className='img' src={FeelsLikeIcon} width={26} height={26} />
+                        <div>
+                            <p>{feelsLikeTemp}°C</p>
+                            <p>Feels like</p>
+                        </div>
+                    </div>
+                    <SeparatorVertical />
+                    <div className='flex-center'>
+                        <img className='img' src={HumidityIcon} width={24} height={24} />
+                        <div>
+                            <p>{humidity}%</p>
+                            <p>Humidity</p>
+                        </div>
+                    </div>
+                </div>
+
             </FormMain>
         </MainContainer>
     )
--- src/pages/weather-view/styles.js
+++ src/pages/weather-view/styles.js
@@ -18,11 +18,31 @@ export const FormMain = styled.form`
     border-radius: .4rem;
     display: flex;
     flex-direction: column;
+    text-align: center;
     h3 {
         font-size: 1.25rem;
         color: ${commonStyles.COLORS.THEME};
         margin: 0 .6rem;
         font-weight: 600;
+    }
+
+    .flex-center {
+        display: flex;
+        align-items: center;
+    }
+    .bottom-sec {
+        display: flex;
+        justify-content: space-evenly;
+
+        p {
+            font-size: .7rem;
+            margin: 0;
+            text-align: left;
+        }
+        .img {
+            margin-right: .5rem;
+        }
+
     }
 
 `
@@ -44,4 +64,27 @@ export const ArrowImg = styled.img`
     color:red;
     fill: red;
     margin-left: 1rem;
+    transform: rotate(-180deg);
+`
+export const WeatherImg = styled.img`
+    margin: auto;
+`
+export const Temp = styled.h2`
+    font-size: 2.5rem;
+    margin: 0 auto;
+`
+export const Desc = styled.p`
+    text-transform: capitalize;
+    font-weight: 500;
+    font-size: .8rem;
+`
+export const Location = styled.p`
+    /* margin: 0 auto; */
+    font-weight: 500;
+    font-size:.8rem;
+`
+export const SeparatorVertical = styled.div`
+    min-height: 100%;
+    width: 1.5px;
+    background-color: rgba(0,0,0,.2);
 `
